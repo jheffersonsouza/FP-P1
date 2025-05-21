54cc4edca6188a9e4255d22db78ec78df97c7ea4
Jhefferson Souza <188925448+jheffersonsouza@users.noreply.github.com>
2025-05-21 09:18:47 -0300

    Aprimorando a funcionalidade de Gráfico.

--- Graph.py
+++ Graph.py
@@ -7,20 +7,27 @@ from matplotlib import pyplot as plt
 
 
 def plot(*votos: int):
-    candidatos = []
-    for i in range(0, len(votos)):
-        candidatos.append(f'C{i + 1}')
+    """
+    Essa função é bem hardcoded já que as variáveis de votos la não estão em uma lista.
+    A ideia de ter criado um arquivo separado foi para facilitar na hora de fazer pullrequest/merge.
 
-    df = pd.DataFrame({'Candidatos': candidatos, 'Votos': votos})
+    :param votos: São as varáveis com os votos recebidos.
+    """
+    df = pd.DataFrame({'Candidatos': ['C1', 'C2', 'C3', 'C4', 'Brancos', 'Nulos'], 'Votos': votos})
 
     # TODO: Procurar um tema bonito.
     sns.set_theme(style="whitegrid")
 
-    plt.figure(figsize = (12, 10))
-    axes = sns.barplot(data=df, x='Candidatos', y='Votos', palette='bright', hue='Votos')
+    # TODO: Mudar esse tamanho também, achar um adequado.
+    plt.figure(figsize=(12, 10))
+    # TODO: Gostaria de aumentar o tamanho do hue também, achei pequeno.
+    axes = sns.barplot(data=df, x='Candidatos', y='Votos', palette='bright', hue='Votos',)
 
+    # TODO: As porcentagens não são escalaveis  com o total de votos, em alguns casos
+    # a y fica muito pequeno ou muito grande. É um problema visual. Teria que fazer relativo
+    # ao tamanho real  da diferença entre as 2 unidades mais proximo.
     for i, v in enumerate(votos):
-        axes.text(i, 1.05 * v , f'{v/sum(votos) * 100:.2f}%', ha='center', va='center')
+        axes.text(i, 1.025 * v, f'{v / sum(votos) * 100:.2f}%', ha='center', va='center')
 
     plt.title('Votos por Candidato', fontsize=16)
     plt.show()
